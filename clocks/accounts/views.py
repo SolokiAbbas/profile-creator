caadf2116d9f345b4c8df8b95d932c7ce8c50efb
Abbas <SolokiAbbas@gmail.com>
2018-01-24 18:56:34 -0500

    added navbar with links and views

--- clocks/accounts/views.py
+++ clocks/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from accounts.forms import UserForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'accounts/index.html')
+
+def register(request):
+    registered = False
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'accounts/register.html',
+                        {'user_form':user_form, 'registered': registered})
