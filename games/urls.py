0b8cf52833de377524a54bdab60c1d5eba0917bc
Abbas <SolokiAbbas@gmail.com>
2018-03-17 21:54:36 -0400

    added collision html

--- games/urls.py
+++ games/urls.py
@@ -5,7 +5,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 app_name = 'games'
 
 urlpatterns = [
-    url(r'^forced/$', views.forced, name='forced')
+    url(r'^forced/$', views.forced, name='forced'),
+    url(r'^collision/$', views.collision, name='collision')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- games/views.py
+++ games/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def forced(request):
     return render(request, 'games/forced.html')
+
+def collision(request):
+    return render(request, 'games/collision.html')
